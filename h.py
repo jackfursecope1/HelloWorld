663642001e53f2ae06b6f0bcbda6df7422d14fe5
Jack Furse-Cope <jack@webarmadillo.net>
2020-11-29 19:32:20 +0000

    adding if to hru code

--- h.py
+++ h.py
@@ -1,3 +1,7 @@
 # asking user how they are 
-input("Hello How are you? ")
-
+hru=int(input("Hello How are you? 1.good or 2. bad "))
+#running an if statement to send message to user depedning upon their input
+if hru == 1:
+	print("Glad, Enjoy the rest of your day")
+if hru == 2:
+	print("cheer up")
