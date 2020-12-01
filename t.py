c596a076a3f5ac698b0fe1ee68866aa95d49a583
Jack Furse-Cope <jack@webarmadillo.net>
2020-12-01 14:33:54 +0000

    added more customisation to user

--- t.py
+++ t.py
@@ -3,13 +3,16 @@
 #asking user what times table they want printed
 user_inp=int(input("Enter a times table you want printed? "))
 
+#ask user how many times
+usertime=int(input("How many times? "))
+
 #main subroutine to print that times table
-def timestable(user_inp):
+def timestable(user_inp,usertime):
 	totlist=[]	
-	for x in range (1,13):
+	for x in range (1,usertime+1):
 		tot=user_inp*x
 		totlist.append(tot)
 	print(totlist)
 
 #running that subroutine
-timestable(user_inp)
+timestable(user_inp,usertime)
